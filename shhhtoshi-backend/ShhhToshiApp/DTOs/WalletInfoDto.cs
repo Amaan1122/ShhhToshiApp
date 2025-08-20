00a9d2b1f8761c0284adbbcc0bab83c3b7029058
Amaan1122 <amaanshaikh898.as@gmail.com>
2025-08-20 23:16:17 +0530

    Implement Server API, Model, DTOs for Task Reward System Functionality

--- shhhtoshi-backend/ShhhToshiApp/DTOs/WalletInfoDto.cs
+++ shhhtoshi-backend/ShhhToshiApp/DTOs/WalletInfoDto.cs
@@ -3,7 +3,7 @@
     public class WalletInfoDto
     {
         public string Address { get; set; }
-        public decimal Balance { get; set; }
+        public decimal TONBalance { get; set; }
         public decimal StakedAmount { get; set; }
         public DateTime LastStakedAt { get; set; }
     }
